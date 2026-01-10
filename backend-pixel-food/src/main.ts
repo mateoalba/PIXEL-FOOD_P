f1b820cf65b5d027c009aeb75d812a8b56a1caa6
msteoalba <mateoalba1234gmail.com>
2026-01-10 02:16:42 -0500

    Ajuste del Main

--- backend-pixel-food/src/main.ts
+++ backend-pixel-food/src/main.ts
@@ -5,11 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, 
-      forbidNonWhitelisted: true, 
+      whitelist: true,
+      forbidNonWhitelisted: true,
       transform: true,
     }),
   );
